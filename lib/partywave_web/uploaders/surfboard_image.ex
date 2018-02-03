0a6f4b36b0ef147be36c13697708be6d9af32cfc
Joshua Cavin <jcavin9@gmail.com>
2018-02-03 09:46:28 -0800

    Initial template design

--- lib/partywave_web/uploaders/surfboard_image.ex
+++ lib/partywave_web/uploaders/surfboard_image.ex
@@ -10,9 +10,9 @@ defmodule PartywaveWeb.SurfboardImage do
   # @versions [:original, :thumb]
 
   # Whitelist file extensions:
-  # def validate({file, _}) do
-  #   ~w(.jpg .jpeg .gif .png) |> Enum.member?(Path.extname(file.file_name))
-  # end
+  def validate({file, _}) do
+    ~w(.jpg .jpeg .gif .png) |> Enum.member?(Path.extname(file.file_name))
+  end
 
   # Define a thumbnail transformation:
   # def transform(:thumb, _) do
