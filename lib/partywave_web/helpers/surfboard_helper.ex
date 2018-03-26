8e5e7d123a338ccb77dd07cf5e49f30f467fdd14
Joshua Cavin <jcavin9@gmail.com>
2018-03-25 21:03:25 -0700

    SEO title tags

--- lib/partywave_web/helpers/surfboard_helper.ex
+++ lib/partywave_web/helpers/surfboard_helper.ex
@@ -7,19 +7,23 @@ defmodule PartywaveWeb.SurfboardHelper do
   end
 
   def reviews_average_rating(average_rating) do
+    rating = if (average_rating == 0), do: "No Review", else: number_to_percent(average_rating)
+
     content_tag :div do
       [
         content_tag(:i, "", class: "fa fa-thermometer-half rating_icon"),
-        content_tag(:span, number_to_percent(average_rating))
+        content_tag(:span, rating)
       ]
     end
   end
 
   def our_rating(rating) do
+    our_rating = if (rating == 0), do: "No Review", else: number_to_percent(rating)
+
     content_tag :div do
       [
         content_tag(:i, "", class: "fa fa-hand-rock-o rating_icon"),
-        content_tag(:span, number_to_percent(rating))
+        content_tag(:span, our_rating)
       ]
     end
   end
