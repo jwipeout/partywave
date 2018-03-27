1b18bed26bf1b50d45a591e8d1d19536b53bb838
Joshua Cavin <jcavin9@gmail.com>
2018-03-26 20:22:10 -0700

    aws

--- config/prod.exs
+++ config/prod.exs
@@ -85,4 +85,6 @@ config :arc,
 
 # ex_aws file storage
 config :ex_aws,
+  access_key_id: [System.get_env("AWS_ACCESS_KEY_ID"), :instance_role],
+  secret_access_key: [System.get_env("AWS_SECRET_ACCESS_KEY"), :instance_role]
   region: "us-west-2"
