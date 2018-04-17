60a3be3e28860e4c97044699da5f4c8da47c65b8
Joshua Cavin <jcavin9@gmail.com>
2018-04-16 18:38:46 -0700

    Force ssl

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ use Mix.Config
 config :partywave, PartywaveWeb.Endpoint,
   load_from_system_env: true,
   url: [scheme: "http", host: "partywave.io", port: 80],
-  # force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
